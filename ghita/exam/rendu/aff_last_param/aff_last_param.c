2f96f42e528c2dd1970d30fe103295d9bd11b142
Ghita Qid <gqid@student.1337.ma>
2024-09-12 00:21:51 +0100

    gqid

---
+++ ghita/exam/rendu/aff_last_param/aff_last_param.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   aff_last_param.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gqid <gqid@student.1337.ma>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/11 22:36:10 by gqid              #+#    #+#             */
+/*   Updated: 2024/09/12 00:18:10 by gqid             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int	main(int ac, char **av)
+{
+	if (ac > 1)
+		ft_putstr(av[ac - 1]);
+	ft_putchar('\n');
+	return (0);
+}
